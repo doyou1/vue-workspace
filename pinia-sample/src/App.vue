f9c0c47c571b3325d541f6fd83ee4b698ab0beb6
doyou1 <tla149@daum.net>
2022-08-30 13:11:04 +0900

    pinia sample setup

---
+++ pinia-sample/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-for="(product, idx) in products" :key="idx">
+    <div>
+      {{product.name}}
+    </div>
+    <div>
+      {{product.image}}
+    </div>
+    <div>
+      {{product.price}}
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+import { storeToRefs } from "pinia";
+import { useProductStore } from "@stores/ProductStore";
+
+export default {
+  data() {
+    return {
+    };
+  },
+  setup() {
+    const store = useProductStore();
+    const { products } = storeToRefs(store);
+    return {
+      products,
+    }
+  }
+};
+</script>
+
+<style></style>
