1616b8f36c4df13aee760fa55c9fe738ef82a61d
doyou1 <tla149@daum.net>
2023-10-04 22:49:55 +0900

    use slot

--- vue-school/fundamental-basic/src/App.vue
+++ vue-school/fundamental-basic/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div id="app" class="content mx-auto py-6">
+  <!-- about vue component concept -->
+  <!-- <div id="app" class="content mx-auto py-6">
     <div class="title">Coffee Plans</div>
     <div class="subText">We travel the world to source the very best single origin coffee for you</div>
     <PlanPicker />
+  </div> -->
+  <!-- about vue slot -->
+  <div>
+    <h1>Vue.js Slots Fundamentals</h1>
+    <TodoItem>
+      Buy Banana
+    </TodoItem>
+    <TodoItem>
+      Celebrate
+    </TodoItem>
   </div>
 </template>
 
 <script setup>
 import PlanPicker from './components/PlanPicker.vue';
+import TodoItem from './components/TodoItem.vue';
 </script>
 
 <style scoped>
---
+++ vue-school/fundamental-basic/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="checkbox" v-model="completed" />
+    <span :class="{done: completed}">
+      <slot ></slot>
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+
+const completed = ref(true);
+
+</script>
+
+<style>
+.done {
+  color: green;
+  text-decoration: line-through;
+}
+
+</style>
