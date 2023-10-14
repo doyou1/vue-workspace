19ace073c550515a3d12aec45a1f14b33d5e1ff4
doyou1 <tla149@daum.net>
2023-10-14 14:20:34 +0900

    memo vue3-express-mariadb sampling complete

--- vue-apps/memo-vue3-express-mariadb/README.md
+++ vue-apps/memo-vue3-express-mariadb/README.md
@@ -3,3 +3,26 @@
 - frontend => vue3
 - backend => express
 - db => mysql
+
+### mariadb onoff with brew
+- init 
+    - brew update
+    - brew install mariadb
+    - brew services start mariadb
+    - mysql.server start
+
+    - mysql -u root
+        - 이걸 했을때 Access denied가 나오면 PW 설정을 안했을 가능성이 큼
+
+    - sudo mariadb-secure-installation
+        - 이걸 해서 다 권장인 Y 를 택하고 (나중에 설정 변경 가능) 비번을 설정한다
+
+    - mysql -u root -p
+        - 이게 나왔을때 내가 설정한 PassWord 를 입력하면 잘 실행됨!!
+    - (id: root, pwd: 0000)
+
+- install : `brew install mariadb`
+- start : `brew services start mariadb` `mysql.server start`
+- stop : `brew services stop mariadb`
+- status : `brew services list`
+
--- vue-apps/memo-vue3-express-mariadb/frontend/src/components/Memo.vue
+++ vue-apps/memo-vue3-express-mariadb/frontend/src/components/Memo.vue
@@ -1,14 +1,53 @@
 <script setup>
+import { onMounted, reactive } from "vue";
+import axios from "axios";
+
+const state = reactive({
+  data: [],
+});
+
+const initData = () => {
+  axios.get("api/memos").then((res) => {
+    state.data = res.data;
+  });
+}
+
+const addData = () => {
+  const content = prompt("내용을 입력해주세요.");
+  if(!content) {
+    alert("입력한 내용이 없습니다.")
+    return;
+  }
+
+  axios.post("api/memos", { content: content }).then((res) => {
+    state.data = res.data;
+  });
+};
+
+const editData = (id) => {
+  const content = prompt("내용을 입력해주세요.", state.data.find((v)=> v.id === id).content);
+
+  if(!content) {
+    alert("입력한 내용이 없습니다.")
+    return;
+  }
+
+  axios.put(`api/memos/${id}`, { content: content }).then((res) => {
+    state.data = res.data;
+  });
+}
+onMounted(()=> {
+  initData();
+})
 </script>
 
 <template>
   <div class="memo">
+    <div class="act">
+      <el-button type="primary" @click="addData">+ 추가</el-button>
+    </div>
     <ul>
-      <li>메모 1 내용</li>
-      <li>메모 2 내용</li>
-      <li>메모 3 내용</li>
-      <li>메모 4 내용</li>
-      <li>메모 5 내용</li>
+      <li v-for="d in state.data" :key="d.id" @click="editData(d.id)">{{ d.content }}</li>
     </ul>
   </div>
 </template>
@@ -23,6 +62,17 @@
 .memo ul li {
   padding: 5px 10px;
   margin: 5px;
+  border: 1px solid #eee;
+  cursor: pointer;
 }
 
+.memo ul li:hover {
+  font-weight: bold;
+}
+
+
+.memo .act {
+  padding: 10px 5px 5px 5px;
+  text-align: right;
+}
 </style>
