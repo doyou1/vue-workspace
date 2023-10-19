fb3dbb692a6f42c0922914e88f504a75a27c133e
doyou1 <tla149@daum.net>
2023-10-20 00:51:57 +0900

    재귀컴포넌트 init

---
+++ slot-lodash-ts-sampling/src/components/Child.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+  name: "ReChildView",  
+};
+</script>
+
+<script setup lang="ts">
+import type { ReData } from '../App.vue';
+
+defineProps<{
+  data: ReData;
+}>();
+</script>
+
+<template>
+  <ul>
+        <li>
+            {{data.name}}
+            <ReChildView v-if="data.child" :data="data.child"></ReChildView>
+        </li> 
+  </ul>
+</template>
+
+<style scoped>
+</style>
