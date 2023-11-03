5478def34725b1ff262738c5798ec27ff4dd7a8a
doyou1 <tla149@daum.net>
2023-11-04 00:09:19 +0900

    reading documents(next time queris)

---
+++ vue-sampling/vue-query-sampling/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div>vue-query sampling</div>
+</template>
+
+<style scoped>
+</style>
---
+++ vue-sampling/vue-query-sampling/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { VueQueryPlugin } from "vue-query";
+
+const app = createApp(App);
+
+app.use(VueQueryPlugin);
+app.mount('#app');
