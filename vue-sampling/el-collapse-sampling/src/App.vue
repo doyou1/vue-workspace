4d05bd246dec85be285b6871e1debaf1061a68e5
doyou1 <tla149@daum.net>
2023-12-06 22:37:46 +0900

    container height fix && item height auto fill

--- vue-sampling/el-collapse-sampling/src/App.vue
+++ vue-sampling/el-collapse-sampling/src/App.vue
@@ -1,7 +1,55 @@
 <template>
   <div class="container">
-    <el-collapse v-model="activeNames" accordion>
-      <el-collapse-item title="item1" name="item1"> 
+    <el-collapse v-model="activeNames" :style="compStyle">
+      <el-collapse-item title="item1" name="item1">
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
         <div>
           Consistent with real life: in line with the process and logic of real
           life, and comply with languages and habits that the users are used to;
@@ -11,14 +59,62 @@
           as: design style, icons and texts, position of elements, etc.
         </div>
       </el-collapse-item>
-      <el-collapse-item title="item2" name="item2"> 
+      <el-collapse-item title="item2" name="item2">
         <div>
-          Operation feedback: enable the users to clearly perceive their
-          operations by style updates and interactive effects;
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
         </div>
         <div>
-          Visual feedback: reflect current state by updating or rearranging
-          elements of the page.
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
+        </div>
+        <div>
+          Consistent with real life: in line with the process and logic of real
+          life, and comply with languages and habits that the users are used to;
+        </div>
+        <div>
+          Consistent within interface: all elements should be consistent, such
+          as: design style, icons and texts, position of elements, etc.
         </div>
       </el-collapse-item>
     </el-collapse>
@@ -26,15 +122,39 @@
 </template>
 
 <script setup>
-import { ElCollapse, ElCollapseItem } from 'element-plus';
-import { ref } from "vue";
+import { ElCollapse, ElCollapseItem } from "element-plus";
+import { computed, ref } from "vue";
 const activeNames = ref(["item1", "item2"]);
 
+const compStyle = computed(() => {
+  if (activeNames.value.length === 0) return undefined;
+  if (activeNames.value.length === 1) return {
+    "--collapse-item-height": "calc(300px - 48px)",
+  };
+  if (activeNames.value.length === 2) return {
+    "--collapse-item-height": "calc(300px / 2)",
+  };
+})
 </script>
 
 <style scoped>
-
 .container {
+  width: 300px;
   height: 300px;
+  border: solid 1px black;
+  overflow-y: hidden;
+
+  .el-collapse {
+    max-height: 300px;
+    overflow-y: hidden;
+    .el-collapse-item {
+      max-height: var(--collapse-item-height);
+      overflow-y: hidden;
+    }
+  }
+  ::v-deep(.el-collapse-item__wrap) {
+    max-height: calc(var(--collapse-item-height) - 48px);
+    overflow-y: scroll;
+  }
 }
 </style>
