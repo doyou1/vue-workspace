87f5216231a21a1c665a3142ca7cebdfc39afbf4
doyou1 <tla149@daum.net>
2024-04-12 15:52:01 +0900

    textarea 줄바꿈 기능(string 데이터 변환 및 출력 시 줄바꿈 표시)

---
+++ text-area-line-change/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ElInput } from 'element-plus';
+import { computed, ref } from 'vue';
+
+const text = ref<string>("");
+
+const computedText = computed(() => {
+  let result = "";
+  [...text.value].forEach((char, i) => {
+    /** Enter key 클릭시 */
+    if (text.value.charCodeAt(i) === 10) {
+      result += "\n"
+    } else {
+      result += char;
+    }
+  })
+
+  return result;
+})
+</script>
+
+<template>
+  <div>
+    <el-input v-model="text" type="textarea" />
+    <div>text: {{ text }}</div>
+    <div class="n-contain">computedText: {{ computedText }}</div>
+  </div>
+</template>
+
+<style scoped>
+.n-contain {
+  white-space: pre-wrap;
+}
+</style>
